1cccf7fb06bd18f6e09305976be1e33b96bd433c
MaleniRayme <34254717+MaleniRayme@users.noreply.github.com>
2017-12-12 01:05:35 -0500

    editado

--- Tutorial GitHub/src/paquete/Tutorial.java
+++ Tutorial GitHub/src/paquete/Tutorial.java
@@ -5,6 +5,7 @@ public class Tutorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buen día docente Omar :v");
+		System.out.println("Trabajo colaborativo");
 	}
 
 }
